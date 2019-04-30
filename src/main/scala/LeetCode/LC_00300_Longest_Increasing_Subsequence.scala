4b2d5094e9e3449b553307ed05a78df7c708aa45
zxcv551133 <zxcv551133@gmail.com>
2019-04-30 19:02:43 +0900

    00300 functional solution for LIS using 'lower_bound' in scala

---
+++ src/main/scala/LeetCode/LC_00300_Longest_Increasing_Subsequence.scala
@@ -0,0 +1,28 @@
+package LeetCode
+
+import scala.annotation.tailrec
+
+object LC_00300_Longest_Increasing_Subsequence {
+  def lengthOfLIS(nums: Array[Int]): Int = {
+    val vc = Vector[Int](Int.MinValue)
+
+
+    @tailrec
+    def lowerBound(v: Vector[Int], x: Int, lo: Int, hi: Int): Int = {
+      if(lo + 1 == hi) return hi
+      val mi = (lo + hi) /2
+      if(v(mi) < x) lowerBound(v, x, mi, hi)
+      else lowerBound(v, x, lo, mi)
+    }
+
+    // TODO 스칼라 immutable vector 의 시간복잡도 시간날때 점검. updated가 eC인 이유  
+    nums.foldLeft(vc)((v, x) => {
+      if(x > v.last) v :+ x
+      else v.updated(lowerBound(v, x, 0, v.length), x)
+    }).length - 1
+  }
+  def main(args: Array[String]): Unit = {
+    println(lengthOfLIS(Array(10,9,2,5,3,7,101,18)))
+    println(lengthOfLIS(Array(2,5,3,4)))
+  }
+}
