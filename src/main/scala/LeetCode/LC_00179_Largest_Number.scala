347e7d54445828dcca1ce5a31a546e42accc5703
zxcv551133 <zxcv551133@gmail.com>
2019-05-03 17:12:46 +0900

    00179 간단한 방법으로 수정

--- src/main/scala/LeetCode/LC_00179_Largest_Number.scala
+++ src/main/scala/LeetCode/LC_00179_Largest_Number.scala
@@ -1,17 +1,12 @@
 package LeetCode
 
-import scala.annotation.tailrec
 
 object LC_00179_Largest_Number {
   def largestNumber(nums: Array[Int]): String = {
-
-//    TODO: deleteLeadingZero 시간복잡도 개선
-    def deleteLeadingZero(x: String): String = {
-      if(x.head != '0' || x.size == 1) x
-      else deleteLeadingZero(x.drop(1))
-    }
     def ord(x: String, y: String): Boolean = x + y > y + x
-    deleteLeadingZero(nums.sortWith((x,y) => ord(x.toString , y.toString)).foldLeft("")(_ + _.toString))
+    val ret = nums.sortWith((x,y) => ord(x.toString , y.toString)).foldLeft("")(_ + _.toString)
+    if(ret.head == '0') "0"
+    else ret
   }
 
   def main(args: Array[String]): Unit = {
