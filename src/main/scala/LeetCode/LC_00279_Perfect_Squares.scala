2189787a83f8400f4923a7d52b16660ec95c2ecb
zxcv551133 <zxcv551133@gmail.com>
2019-05-08 18:23:56 +0900

    memoization 두가지 방법
    약간의 차이

--- src/main/scala/LeetCode/LC_00279_Perfect_Squares.scala
+++ src/main/scala/LeetCode/LC_00279_Perfect_Squares.scala
@@ -7,55 +7,38 @@ object LC_00279_Perfect_Squares {
 
 
 
-//  def memoize[A, B](func: A => B) = new (A => B){
-//    val cache = scala.collection.mutable.Map[A, B]()
-//    def apply(x: A): B = cache.getOrElseUpdate(x, func(x))
-//  }
-
-
-
-//  lazy val fib: Int => BigInt = memoize {
-//    case 0 => 0
-//    case 1 => 1
-//    case n => fib(n-1) + fib(n-2)
-//  }
-
-
-//  lazy val c: ((Int, Int)) => BigInt = memoize {
-//    case (_, 0) => 1
-//    case (n, r) if r > n/2 => c(n, n - r)
-//    case (n, r) => c(n - 1, r - 1) + c(n - 1, r)
-//  }
-//  def memoization[I, O](f: I => O): I => O = new scala.collection.mutable.HashMap[I, O]() {
-//    override def apply(key: I): O = getOrElseUpdate(key, f(key))
-//  }
 
   // TODO: 더 빠르게 변경
   def numSquares(n: Int): Int = {
+
+
+//    // TODO memoize 다른 방법 활용하는 법
+//    def memoization[A, B](func: A => B) = new (A => B){
+//      val cache = scala.collection.mutable.Map[A, B]()
+//      def apply(x: A): B = cache.getOrElseUpdate(x, func(x))
+//    }
+
+//  lazy val pp: Int => Int = memoization {
+//    case 0 => 0
+//    case x: Int => Range(math.sqrt(x).toInt, 1, -1).foldLeft(x)((a , b) => math.min(pp(x - b * b) + 1, a))
+//  }
+//    pp(n)
+
+
+
     def memoize[I, O](f: I => O): I => O = new scala.collection.mutable.HashMap[I, O]() {
       override def apply(key: I): O = getOrElseUpdate(key, f(key))
     }
-//    val dp = collection.mutable.Map[Int, Int]()
-//    dp.put(0, 0)
 
     lazy val numSq: Int => Int = memoize {
       case 0 => 0
-      case x => (1 to math.sqrt(x).toInt).foldLeft(x)((a , b) => math.min(numSq(x - b * b) + 1, a))
+      case x => Range(math.sqrt(x).toInt, 1, -1).foldLeft(x)((a, b) => math.min(numSq(x - b * b) + 1, a))
+      // you will see "Stack Over Flow" if you implement case expression as
+      // case x => (1 to math.sqrt(x).toInt).foldLeft(x)((a , b) => math.min(numSq(x - b * b) + 1, a))
+      // if you want to use this, then you should add "(1 to n).foreach(numSq)" before numSq(n).
     }
-
-//    println(fib(300))
-    (1 to n).foreach(numSq)
-
-    //    Range(1, n, 100).foreach(numSq)
-
     numSq(n)
 
-//
-//    def buildDp(x: Int): Unit = {
-//      (1 to x).filter(v => v * v <= x).foreach(y => dp.put(x, math.min(dp.getOrElse(x, x), 1 + dp(x - y * y))))
-//    }
-//    (1 to n).foreach(buildDp)
-//    dp(n)
   }
 
   def main(args: Array[String]): Unit = {
